6fdb3fd8022d71afb0d1fc741fda6fe5c5bc1b05
Rakib Hasan <123249343+Rakib-788@users.noreply.github.com>
2023-02-03 21:11:40 +0600

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ from os import getenv
 TOKEN = getenv("TOKEN", "5904590628:AAEc-4vlJg9LTzPF94hON0CX4Bv7dnqkXHM")
 MONGO_DB_URL = getenv(
     "MONGO_DB_URL",
-    "mongodb+srv://bsdk:betichod@cluster0.fgj1r9z.mongodb.net/?retryWrites=true&w=majority",
+    "mongodb+srv://Nixa:Nixa@cluster0.lrpyzz4.mongodb.net/?retryWrites=true&w=majority",
 )
